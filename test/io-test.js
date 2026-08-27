32db6b8479a265458ebbea044b174bcef9431324
c0stra <ondrej.fischer@gmail.com>
2026-08-28 00:02:46 +0200

    Progress in unit tests

--- test/io-test.js
+++ test/io-test.js
@@ -1,16 +1,27 @@
-import {assertEquals, output, suite} from "./run.js";
-import {state} from "../mvc.js";
+import {assert, output, suite} from "./run.js";
+import {state, transform} from "../mvc.js";
 import {get} from "../io.js";
+import {div} from "../html.js";
 
 suite({
 
     name: "MVC test suite",
 
-    testState() {
+    async Get_request_should_set_response_to_output_state() {
         let testState = state()
         get("data.json", testState).observeUrl()
         output("Test state: ", testState.numericField)
-        assertEquals(40, testState.numericField.get())
+        await assert(() => 40 === testState.numericField.get())
     },
 
+    async Get_request_output_should_properly_transform() {
+        let testState = state()
+        let transformedState = transform(testState, value => value?.numericField * 10)
+        get("data.json", testState).observeUrl()
+        output(
+            div("Test state: ", testState.numericField),
+            div("Transformed state: ", transformedState)
+        )
+        await assert(() => 400 === transformedState.get())
+    }
 })
--- test/run.js
+++ test/run.js
@@ -9,8 +9,7 @@ export function suite(def) {
         .entries(def)
         .filter(testMethods)
         .flatMap(testCases(def))
-        .map(runTestCase)
-        .forEach(rep);
+        .forEach(testCase => runTestCase(testCase).finally(() => rep(testCase)));
 }
 
 function testMethods(entry) {
@@ -28,18 +27,18 @@ function testCase(name, scenario, parameters) {
     return {
         name: name,
         parameters: parameters,
-        run() {
-            if(Array.isArray(parameters)) scenario(...parameters)
-            else scenario(parameters)
+        async run() {
+            if(Array.isArray(parameters)) await scenario(...parameters)
+            else await scenario(parameters)
         }
     }
 }
 
-function runTestCase(testCase) {
+async function runTestCase(testCase) {
     testCase.start = new Date()
     try {
         currentTest = testCase.name
-        testCase.run()
+        await testCase.run().catch(error => testCase.error = error)
     } catch (error) {
         testCase.error = error
     }
@@ -49,22 +48,21 @@ function runTestCase(testCase) {
 
 function report(def) {
     let s = document.body.appendChild(document.createElement("div"))
+    s.setAttribute("class", "suite")
     s.appendChild(document.createElement("h1")).appendChild(document.createTextNode(def.name))
     let tb = s.appendChild(document.createElement("table")).appendChild(document.createElement("tbody"))
     let thr = tb.appendChild(document.createElement("tr"))
     thr.appendChild(document.createElement("th")).appendChild(document.createTextNode("TestCase"))
-    thr.appendChild(document.createElement("th")).appendChild(document.createTextNode("Parameters"))
-    thr.appendChild(document.createElement("th")).appendChild(document.createTextNode("Result"))
     thr.appendChild(document.createElement("th")).appendChild(document.createTextNode("Start"))
     thr.appendChild(document.createElement("th")).appendChild(document.createTextNode("End"))
     thr.appendChild(document.createElement("th")).appendChild(document.createTextNode("Error"))
     return testCase => {
         let t = tb.appendChild(document.createElement("tr"))
-        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.name))
-        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.parameters || ""))
-        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.error ? "failed" : "passed"))
-        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.start))
-        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.end))
+        let tn = t.appendChild(document.createElement("td"))
+        tn.appendChild(document.createElement("strong").appendChild(document.createTextNode(testCase.name)))
+        if(testCase.parameters) tn.appendChild(document.createTextNode(testCase.parameters))
+        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.start.toISOString()))
+        t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.end.toISOString()))
         t.appendChild(document.createElement("td")).appendChild(document.createTextNode(testCase.error || ""))
         t.setAttribute("class", testCase.error ? "failed" : "passed")
     }
@@ -76,6 +74,23 @@ export function assertEquals(expected, actual) {
 
 export function output(...children) {
     let out = document.body.appendChild(document.createElement("div"))
-    out.appendChild(document.createElement("h3")).appendChild(document.createTextNode(currentTest + " output"))
-    children.forEach(child => out.appendChild(node(child)))
+    out.appendChild(document.createElement("h3")).appendChild(document.createTextNode("Output of: " + currentTest))
+    out.setAttribute("class", "output-box")
+    let cnt = out.appendChild(document.createElement("div"))
+    cnt.setAttribute("class", "output")
+    children.forEach(child => cnt.appendChild(node(child)))
+}
+
+export async function assert(predicate) {
+    return  new Promise((resolve, reject) => retry(predicate, 3, resolve, reject))
+}
+
+function retry(predicate, remaining, resolve, reject) {
+    if(remaining < 1) reject(new Error("Failed"))
+    if(predicate())
+        resolve()
+    else {
+        console.log("Retrying...")
+        setTimeout(retry, 200, predicate, remaining - 1, resolve, reject)
+    }
 }
